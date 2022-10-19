d1c60848ed4754322444a56f3aaa260620312ba9
“withsjb” <withsjb1@gmail.com>
2022-10-19 23:15:44 +0900

    coins

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,6 +16,10 @@ export default function Navbar() {
         <Link href="/profile">
           <a>Profile</a>
         </Link>
+
+        <Link href="/coins">
+          <a>coins</a>
+        </Link>
       </div>
     </div>
   );
